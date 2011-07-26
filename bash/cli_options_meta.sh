a1b5943e287d48a918c9270862a3e544c3804b8c
Colin MacDonald <colin@bluegraybox.com>
2011-07-25 21:57:17 -0400

    Wrapped getopts in a function.
    Lots of tinkering with passing assoc arrays and params.

--- bash/cli_options_meta.sh
+++ bash/cli_options_meta.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
-declare -A flags values
-flags=([d]=debug [h]=show_help)
-values=([f]=foo [b]=bar)
-flag_opt=""
-value_opt=""
-for x in "${!flags[@]}" ; do flag_opt="$flag_opt$x" ; done
-for x in "${!values[@]}" ; do value_opt="$value_opt$x:" ; done
+function get_my_opts () {
+    # echo "param count=${#@}"
+    # echo "params=" "$@"
+    # for x in "$@" ; do echo "param=$x" ; done
+    local -A _flags=$1 _values=$2
+    shift 2
+    local flag_opt=""
+    local value_opt=""
+    local x
+    for x in "${!_flags[@]}" ; do flag_opt="$flag_opt$x" ; done
+    for x in "${!_values[@]}" ; do value_opt="$value_opt$x:" ; done
+    # echo "flag_opt=$flag_opt, value_opt=$value_opt"
 
-while getopts ":$flag_opt$value_opt" option ; do
-    case $option in
-        \?) echo "Unknown option -$OPTARG" ;;
-        [$flag_opt])
-            x=${flags[$option]}
-            declare $x=${!x:-0}
-            declare $x=$((${!x} + 1)) ;;
-        [$value_opt]) declare ${values[$option]}="$OPTARG" ;;
-    esac
-done
-shift $((OPTIND - 1))
+    while getopts ":$flag_opt$value_opt" option "$@" ; do
+        case $option in
+            \?) echo "Unknown option -$OPTARG" ;;
+            # *) echo "option=$OPTARG" ;;&
+            [$flag_opt])
+                x=${_flags[$option]}
+                local $x=${!x:-0}
+                local $x=$((${!x} + 1)) ;;
+            [$value_opt]) local ${_values[$option]}="$OPTARG" ;;
+        esac
+    done
+    shift $((OPTIND - 1))
 
-echo Flags
-for x in "${flags[@]}" ; do echo "$x=${!x}" ; done
-echo Values
-for x in "${values[@]}" ; do echo "$x=${!x}" ; done
+    echo Flags
+    for x in "${_flags[@]}" ; do echo "$x=${!x}" ; done
+    echo Values
+    for x in "${_values[@]}" ; do echo "$x=${!x}" ; done
+}
 
+function main () {
+    local -A flags values
+    flags="([d]=debug [h]=show_help)"
+    values="([f]=foo [b]=bar)"
+    get_my_opts "$flags" "$values" "$@"
+}
+main "$@"
