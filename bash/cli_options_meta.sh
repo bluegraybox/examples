f599c9c7e03d66cd3b06449899cbb5e1010eaa56
Colin MacDonald <colin@bluegraybox.com>
2011-07-26 04:33:52 -0400

    get_my_opts now actually works as expected.
    Changed local to export, to make variables available.
    Return OPTIND to indicate consumed arguments.
    Removed debugging, added comments.

--- bash/cli_options_meta.sh
+++ bash/cli_options_meta.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 
 function get_my_opts () {
-    # echo "param count=${#@}"
-    # echo "params=" "$@"
-    # for x in "$@" ; do echo "param=$x" ; done
+    # $1 and $2 are strings that evaluate to associative arrays.
+    # For example, "([f]=foo [b]=bar)"
+    # They match options to variables, so the option "-f baz" causes the variable foo to be set to "baz".
+    # Flag options don't take a parameter; we just count how often they appear.
+    # So if the above example were the flags option, "-f -f -f" would cause the variable foo to be set to 3.
     local -A _flags=$1 _values=$2
     shift 2
-    local flag_opt=""
-    local value_opt=""
-    local x
+    local flag_opt="" value_opt="" x
     for x in "${!_flags[@]}" ; do flag_opt="$flag_opt$x" ; done
     for x in "${!_values[@]}" ; do value_opt="$value_opt$x:" ; done
-    # echo "flag_opt=$flag_opt, value_opt=$value_opt"
 
     while getopts ":$flag_opt$value_opt" option "$@" ; do
         case $option in
             \?) echo "Unknown option -$OPTARG" ;;
-            # *) echo "option=$OPTARG" ;;&
             [$flag_opt])
                 x=${_flags[$option]}
-                local $x=${!x:-0}
-                local $x=$((${!x} + 1)) ;;
-            [$value_opt]) local ${_values[$option]}="$OPTARG" ;;
+                # 'export' makes these visible outside this function; otherwise, use 'local'
+                export $x=${!x:-0}
+                export $x=$((${!x} + 1)) ;;
+            [$value_opt]) export ${_values[$option]}="$OPTARG" ;;
         esac
     done
-    shift $((OPTIND - 1))
-
-    echo Flags
-    for x in "${_flags[@]}" ; do echo "$x=${!x}" ; done
-    echo Values
-    for x in "${_values[@]}" ; do echo "$x=${!x}" ; done
+    return $OPTIND
 }
 
 function main () {
-    local -A flags values
-    flags="([d]=debug [h]=show_help)"
-    values="([f]=foo [b]=bar)"
+    # Strings that evaluate to associative arrays
+    local flags="([d]=debug [h]=show_help)" values="([f]=foo [b]=bar)"
     get_my_opts "$flags" "$values" "$@"
+    local used=$?  # grab the return value
+    shift $((used - 1))
+    for x in debug show_help foo bar ; do echo "$x=${!x}" ; done
+    echo "args=$@"
 }
+
 main "$@"
+
