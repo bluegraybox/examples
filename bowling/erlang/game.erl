697cf60fd1879cf8de0b2d8e0c58ee870f40dd12
Colin MacDonald <colin@bluegraybox.com>
2011-10-11 23:13:16 -0400

    Fix edge cases with bad data in bonus rolls.
    Previously, strike and spare bonus rolls were added to the score
    without checking for illegal values. (Normally, this would only be a
    problem in the final frame; otherwise, they would trigger an error
    when their own frame was scored.) Adding these checks and handling
    incomplete game conditions made this complex enough to split out into
    separate functions.
    
    Added unit tests for these conditions.

--- bowling/erlang/game.erl
+++ bowling/erlang/game.erl
@@ -1,8 +1,7 @@
 -module(game).
 -export([score/1]).
 
-score(Rolls) ->
-    frame(1, 0, Rolls).
+score(Rolls) -> frame(1, 0, Rolls).
 
 %% frame/3 takes Frame #, Score accumulator, and list of remaining Rolls.
 %%   It tail-recurses to the next frame with an updated Score and Rolls list.
@@ -12,10 +11,9 @@ frame(11, Score, _BonusRolls) -> Score;
 
 %% Strike.
 frame(Frame, Score, [10|Rest]) ->
-    case Rest of
-        [] -> Score + 10;
-        [Bonus1] -> frame(Frame + 1, Score + 10 + Bonus1, Rest);
-        [Bonus1, Bonus2|_] -> frame(Frame + 1, Score + 10 + Bonus1 + Bonus2, Rest)
+    case strike_bonus(Rest) of
+        err -> err;
+        Bonus -> frame(Frame + 1, Score + 10 + Bonus, Rest)
     end;
 
 %% Bad input.
@@ -24,9 +22,9 @@ frame(_Frame, _Score, [First,Second|_Rest]) when (First + Second > 10) -> err;
 
 %% Spare.
 frame(Frame, Score, [First,Second|Rest]) when (First + Second == 10) ->
-    case Rest of
-        [] -> Score + 10;
-        [Bonus1|_] -> frame(Frame + 1, Score + 10 + Bonus1, Rest)
+    case spare_bonus(Rest) of
+        err -> err;
+        Bonus -> frame(Frame + 1, Score + 10 + Bonus, Rest)
     end;
 
 %% Normal.
@@ -36,4 +34,17 @@ frame(Frame, Score, [First,Second|Rest]) ->
 %% Incomplete frames.
 frame(_Frame, Score, [First]) -> Score + First;
 frame(_Frame, Score, []) -> Score.
+
+
+%% spare & strike bonus calculations, with validation.
+spare_bonus([]) -> 0;
+spare_bonus([First|_Rest]) when (First > 10) -> err;
+spare_bonus([First|_Rest]) -> First.
+
+strike_bonus([]) -> 0;
+strike_bonus([10]) -> 10;
+strike_bonus([10,Second|_Rest]) when (Second > 10) -> err;
+strike_bonus([10,Second|_Rest]) -> 10 + Second;
+strike_bonus([First,Second|_Rest]) when (First + Second > 10) -> err;
+strike_bonus([First,Second|_Rest]) -> First + Second.
 
