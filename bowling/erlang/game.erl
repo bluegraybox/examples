0fc705e4b7ae3b8d2da68dec56d24841cce0c2b4
Colin MacDonald <colin@bluegraybox.com>
2011-10-15 15:32:17 -0400

    bowling: Add validation of all rolls.
    Added validate/1 to assert that no individual roll is
    invalid. Simplified other functions which no longer have to
    check. Added unit tests.

--- bowling/erlang/game.erl
+++ bowling/erlang/game.erl
@@ -1,7 +1,15 @@
 -module(game).
 -export([score/1]).
 
-score(Rolls) -> frame(1, 0, Rolls).
+score(Rolls) ->
+    case validate(Rolls) of
+        ok -> frame(1, 0, Rolls);
+        _ -> err
+    end.
+
+validate([First|_Rest]) when (First > 10) or (First < 0) -> err;
+validate([_First|Rest]) -> validate(Rest);
+validate([]) -> ok.
 
 %% frame/3 takes Frame #, Score accumulator, and list of remaining Rolls.
 %%   It tail-recurses to the next frame with an updated Score and Rolls list.
@@ -16,16 +24,12 @@ frame(Frame, Score, [10|Rest]) ->
         Bonus -> frame(Frame + 1, Score + 10 + Bonus, Rest)
     end;
 
-%% Bad input.
-frame(_Frame, _Score, [First|_Rest]) when (First > 10) -> err;
+%% Bad input (*after* strike).
 frame(_Frame, _Score, [First,Second|_Rest]) when (First + Second > 10) -> err;
 
 %% Spare.
 frame(Frame, Score, [First,Second|Rest]) when (First + Second == 10) ->
-    case spare_bonus(Rest) of
-        err -> err;
-        Bonus -> frame(Frame + 1, Score + 10 + Bonus, Rest)
-    end;
+    frame(Frame + 1, Score + 10 + spare_bonus(Rest), Rest);
 
 %% Normal.
 frame(Frame, Score, [First,Second|Rest]) ->
@@ -38,12 +42,10 @@ frame(_Frame, Score, []) -> Score.
 
 %% spare & strike bonus calculations, with validation.
 spare_bonus([]) -> 0;
-spare_bonus([First|_Rest]) when (First > 10) -> err;
 spare_bonus([First|_Rest]) -> First.
 
 strike_bonus([]) -> 0;
 strike_bonus([Only]) -> Only;
-strike_bonus([10,Second|_Rest]) when (Second > 10) -> err;
 strike_bonus([10,Second|_Rest]) -> 10 + Second;
 strike_bonus([First,Second|_Rest]) when (First + Second > 10) -> err;
 strike_bonus([First,Second|_Rest]) -> First + Second.
