7db027712cd04c351ccdcfb8b8ab88c2e441171e
Colin MacDonald <colin@primeradiant.com>
2012-08-11 17:08:46 -0400

    parse_indents.erl: Converted records to tuples.

--- indent-parser/parse_indents.erl
+++ indent-parser/parse_indents.erl
@@ -10,32 +10,31 @@
 -export([main/1, show_path/1, show_indented/1, build_nodes/3]).
 %% Needs to be compiled so we can specify our output functions.
 -mode(compile).
--record(line, {content, indent}).
--record(node, {content, children}).
 
 
 parse_text_line(Text) ->
     %% converts "    text" to (:content "text" :indent 4)
+    %% strip spaces (by default) and tabs
     Content = string:strip(string:strip(Text, left), left, $\t),
     Indent = length(Text) - length(Content),
-    #line{content=string:strip(Content, right, $\n), indent=Indent}.
+    %% Don't strip the line break before we've counted the indent
+    {string:strip(Content, right, $\n), Indent}.
 
 
 build_nodes(Parent, Group, []) -> Parent ! {Group, []};
-build_nodes(Parent, Group, [First|Rest]) ->
+build_nodes(Parent, Group, [{Content, Indent}|Rest]) ->
     %% split off our children from the rest of the Lines.
     %% the first line with an indent =< ours is a sibling, not a child
-    IsChild = fun(L) -> L#line.indent > First#line.indent end,
+    IsChild = fun({_C, I}) -> I > Indent end,
     {ChildLines, SiblingLines} = lists:splitwith(IsChild, Rest),
     spawn(?MODULE, build_nodes, [self(), children, ChildLines]),
     spawn(?MODULE, build_nodes, [self(), siblings, SiblingLines]),
-    receive
-        {children, Children} ->
-            Node = #node{content=First#line.content, children=Children}
+    Node = receive
+        {children, Children} -> {Content, Children}
     end,
     receive
         {siblings, Siblings} ->
-            io:fwrite( "[~s] ~p~n", [Node#node.content, self()]),
+            io:fwrite( "[~s] ~p~n", [Content, self()]),
             Parent ! {Group, [Node|Siblings]}
     end.
 
@@ -44,25 +43,24 @@ build_nodes(Parent, Group, [First|Rest]) ->
 show_indented(Nodes) -> show_indented(Nodes, 0).
 show_indented(Nodes, Indent) ->
     %% print out the contents of a tree of nodes, indented appropriately
-    Show = fun(Node) ->
-        io:fwrite("~s~n", [string:chars($\s, Indent, Node#node.content)]),
-        show_indented(Node#node.children, Indent + 4)
+    Show = fun({Content, Children}) ->
+        io:fwrite("~s~n", [string:chars($\s, Indent, Content)]),
+        show_indented(Children, Indent + 4)
     end,
     lists:map(Show, Nodes).
 
-get_node_path(Node, "") -> Node#node.content;
-get_node_path(Node, ParentPath) ->
-    Content = Node#node.content,
+get_node_path({Content, _Children}, "") -> Content;
+get_node_path({Content, _Children}, ParentPath) ->
     string:join([ParentPath, Content], ":").
 
 show_path(Nodes) -> show_path(Nodes, "").
 show_path(Nodes, ParentPath) ->
     %% print out the leaves of a tree with their parent path (root::parent::child::leaf)
-    Show = fun(Node) ->
-        NodePath = get_node_path(Node, ParentPath),
-        case Node#node.children of
+    Show = fun({Content, Children}) ->
+        NodePath = get_node_path({Content, Children}, ParentPath),
+        case Children of
             [] -> io:fwrite("~s~n", [NodePath]);
-            Children -> show_path(Children, NodePath)
+            _ -> show_path(Children, NodePath)
         end
     end,
     lists:map(Show, Nodes).
@@ -70,8 +68,8 @@ show_path(Nodes, ParentPath) ->
 get_all_lines(Stream) -> get_all_lines(Stream, []).
 get_all_lines(Stream, Lines) ->
     case io:get_line(Stream, "") of
-        eof -> file:close(Stream), Lines;
-        Line -> get_all_lines(Stream, Lines ++ [Line])
+        eof -> lists:reverse(Lines);
+        Line -> get_all_lines(Stream, [Line|Lines])
     end.
 
 process_stream(Stream, ShowFunc) ->
@@ -87,14 +85,16 @@ process_file("-", ShowFunc) ->
     process_stream(standard_io, ShowFunc);
 process_file(Filename, ShowFunc) ->
     case file:open(Filename, [read]) of
-        {ok, Handle} -> process_stream(Handle, ShowFunc);
+        {ok, Handle} ->
+            process_stream(Handle, ShowFunc),
+            file:close(Handle);
         {error, Reason} -> io:fwrite("Error opening file ~s: ~s~n", [Filename, Reason])
     end.
 
 %%--------------------------------------------------------------------------
 
 main([]) ->
-    io:format("usage: ~s.erl [[:module]:display_function] <filenames...>~n", [?MODULE]),
+    io:format("usage: ~s.erl [-f <display_function>] <filenames...>~n", [?MODULE]),
     halt(1);
 main(["-f", FuncName|Filenames]) ->
     Func = list_to_atom(FuncName),
