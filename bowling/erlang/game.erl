8bbfb39df4e3a7bc089e9ce5812f728d05bf715b
Colin MacDonald <colin@primeradiant.com>
2013-02-02 19:40:53 -0500

    Improved error reporting and validation.
    Centralized validation as first clauses of frame/3.
    Report invalid rolls and totals as separate error types; include bad values in return.
    Updated tests to match; report test number in failures.
    Made game_test.erl an escript.

--- bowling/erlang/game.erl
+++ bowling/erlang/game.erl
@@ -2,30 +2,25 @@
 -export([score/1]).
 
 score(Rolls) ->
-    case validate(Rolls) of
-        ok -> frame(1, 0, Rolls);
-        _ -> err
-    end.
-
-validate([First|_Rest]) when (First > 10) or (First < 0) -> err;
-validate([_First|Rest]) -> validate(Rest);
-validate([]) -> ok.
+    frame(1, 0, Rolls).
 
 %% frame/3 takes Frame #, Score accumulator, and list of remaining Rolls.
 %%   It tail-recurses to the next frame with an updated Score and Rolls list.
+
+%% Bad input - do this first so we check post-game bonus rolls.
+frame(_Frame, _Score, [First|_Rest]) when (First < 0) or (First > 10) ->
+    {invalid_roll, First};
+frame(_Frame, _Score, [_First, Second|_Rest]) when (Second < 0) or (Second > 10) ->
+    {invalid_roll, Second};
+frame(_Frame, _Score, [First,Second|_Rest]) when (First /= 10) and (First + Second > 10) ->
+    {invalid_total, First + Second};
 
 % Game complete.
 frame(11, Score, _BonusRolls) -> Score;
 
 %% Strike.
 frame(Frame, Score, [10|Rest]) ->
-    case strike_bonus(Rest) of
-        err -> err;
-        Bonus -> frame(Frame + 1, Score + 10 + Bonus, Rest)
-    end;
-
-%% Bad input (*after* strike).
-frame(_Frame, _Score, [First,Second|_Rest]) when (First + Second > 10) -> err;
+    frame(Frame + 1, Score + 10 + strike_bonus(Rest), Rest);
 
 %% Spare.
 frame(Frame, Score, [First,Second|Rest]) when (First + Second == 10) ->
@@ -47,6 +42,4 @@ spare_bonus([First|_Rest]) -> First.
 strike_bonus([]) -> 0;
 strike_bonus([Only]) -> Only;
 strike_bonus([10,Second|_Rest]) -> 10 + Second;
-strike_bonus([First,Second|_Rest]) when (First + Second > 10) -> err;
 strike_bonus([First,Second|_Rest]) -> First + Second.
-
--- bowling/erlang/game_test.erl
+++ bowling/erlang/game_test.erl
@@ -1,36 +1,44 @@
--module(game_test).
--import(game, [score/1]).
--export([test/0]).
+#!/usr/local/bin/escript
+%% -*- erlang -*-
+%%! -smp enable
 
-test() ->
+-module(game_test).
+%% Needs to be compiled so we can specify our output functions.
+-mode(compile).
+
+-import(game, [score/1]).
+%%-export([test/0]).
+
+main([]) ->
     test([
-        [0,   [0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0]],
-        [20,  [1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]],
-        [err, [1,1, 12,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]],  % invalid roll
-        [err, [1,1, 6,-1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]],  % invalid roll
-        [6,   [1,1, 1,1, 1,1]], %% incomplete
-        [18,  [1,1, 6,4, 3]], %% incomplete w/ spare
-        [150, [5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5]],
-        [47,  [1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 10, 10 ,9]],
-        [173, [7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 10]],
-        [300, [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10]],
-        [280, [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  5]],  % incomplete
-        [300, [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10, 10, 10, 10]], %% extras
-        [err, [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  9,2]],  % invalid extras
-        [err, [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  11]],  % invalid extras
-        [err, [10,  10,  10,  10,  10,  10,  10,  10,  10,  9,1, 11]],  % invalid extras
-        [240, [10,  10,  10,  0,0, 10,  10,  10,  10,  10,  10,  10,  10]],
-        [245, [10,  10,  10,  10,  10,  10,  10,  10,  10,  1,1]],
-        [err, [10,  10,  5,6, 10,  10,  10,  10,  10,  10,  1,1]]]).
+        {0,   [0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0]},
+        {20,  [1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]},
+        {{invalid_roll, 12}, [1,1, 12,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]},  % invalid roll
+        {{invalid_roll, -1}, [1,1, 6,-1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]},  % invalid roll
+        {{invalid_roll, -1}, [1,1, -1,6, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]},  % invalid roll
+        {6,   [1,1, 1,1, 1,1]}, %% incomplete
+        {18,  [1,1, 6,4, 3]}, %% incomplete w/ spare
+        {150, [5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5,5, 5]},
+        {47,  [1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 10, 10 ,9]},
+        {173, [7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 7,3, 10]},
+        {300, [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10]},
+        {280, [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  5]},  % incomplete
+        {300, [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10, 10, 10, 10]}, %% extras
+        {{invalid_total, 11}, [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  9,2]},  % invalid extras
+        {{invalid_roll, 11}, [10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  10,  11]},  % invalid extras
+        {{invalid_roll, 11}, [10,  10,  10,  10,  10,  10,  10,  10,  10,  9,1, 11]},  % invalid extras
+        {240, [10,  10,  10,  0,0, 10,  10,  10,  10,  10,  10,  10,  10]},
+        {245, [10,  10,  10,  10,  10,  10,  10,  10,  10,  1,1]},
+        {{invalid_total, 11}, [10,  10,  5,6, 10,  10,  10,  10,  10,  10,  1,1]}]).
 
 test(Tests) -> test(0, 0, Tests).
 test(Pass, 0, []) -> io:fwrite("~nPassed! ~p tests~n", [Pass]);
 test(Pass, Fail, []) -> io:fwrite("~nFailed! ~p fail, ~p pass~n", [Fail, Pass]);
-test(Pass, Fail, [[Expected, Rolls]|Tests]) ->
+test(Pass, Fail, [{Expected, Rolls}|Tests]) ->
     case score(Rolls) of
         Expected -> io:fwrite("."),
             test(Pass + 1, Fail, Tests);
-        Scored -> io:fwrite("~nFail: expected=~p, scored=~p~n", [Expected, Scored]),
+        Scored -> io:fwrite("~nFailed test ~p: expected=~p, scored=~p~n", [Pass + Fail, Expected, Scored]),
             test(Pass, Fail + 1, Tests)
     end.
 
