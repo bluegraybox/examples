1afcbc8e40754267e7604d35e87c8b7ae8f967be
Colin MacDonald <colin.macdonald@capitalone.com>
2014-08-07 12:55:58 -0400

    Made Recursor.java tail-recursive
    Added comments to recurse.erl.

--- misc_erlang/recursion/Recursor.java
+++ misc_erlang/recursion/Recursor.java
@@ -4,11 +4,15 @@ public class Recursor {
     }
 
     private static int countdown(int x) {
+        return countdown(x, 0);
+    }
+
+    private static int countdown(int x, int total) {
         if (0 == x) {
-            return 1/0;
+            return 1/0;  // generate exception
         }
         else {
-            return 1 + countdown(x - 1);
+            return countdown(x - 1, total + 1);  // recurse with decreased countdown and increased total
         }
     }
 }
--- misc_erlang/recursion/recurse.erl
+++ misc_erlang/recursion/recurse.erl
@@ -6,6 +6,7 @@ countdown() ->
     countdown(100, 0).
 
 countdown(0, Total) ->
-    Total/0;
+    Total/0;  %% generate error
 countdown(Val, Total) ->
+    %% recurse with decreased countdown and increased total
     countdown(Val - 1, Total + 1).
