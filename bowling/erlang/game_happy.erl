2d7801d9202cabc8110a97d3650a27d0ac5c75fe
Colin MacDonald <colin.r.macdonald@gmail.com>
2020-05-06 21:41:31 -0400

    Rename 'frame' function to 'score'

--- bowling/erlang/game_happy.erl
+++ bowling/erlang/game_happy.erl
@@ -3,22 +3,22 @@
 -module(game_happy).
 -export([score/1]).
 
-score(Rolls) -> frame(1, 0, Rolls).
+score(Rolls) -> score(1, 0, Rolls).
 
 % Game complete.
-frame(11, Score, _BonusRolls) -> Score;
+score(11, Score, _BonusRolls) -> Score;
 
 %% Strike.
-frame(Frame, Score, [10|Rest]) ->
-    frame(Frame + 1, Score + 10 + strike_bonus(Rest), Rest);
+score(Frame, Score, [10|Rest]) ->
+    score(Frame + 1, Score + 10 + strike_bonus(Rest), Rest);
 
 %% Spare.
-frame(Frame, Score, [First,Second|Rest]) when (First + Second == 10) ->
-    frame(Frame + 1, Score + 10 + spare_bonus(Rest), Rest);
+score(Frame, Score, [First,Second|Rest]) when (First + Second == 10) ->
+    score(Frame + 1, Score + 10 + spare_bonus(Rest), Rest);
 
 %% Normal.
-frame(Frame, Score, [First,Second|Rest]) ->
-    frame(Frame + 1, Score + First + Second, Rest).
+score(Frame, Score, [First,Second|Rest]) ->
+    score(Frame + 1, Score + First + Second, Rest).
 
 %% spare & strike bonus calculations.
 spare_bonus([First|_Rest]) -> First.
