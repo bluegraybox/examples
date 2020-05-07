044978ff64679fee4e4c2f7c0c35fa0325a0c67d
Colin MacDonald <colin.r.macdonald@gmail.com>
2020-05-06 20:29:26 -0400

    Change parameter order to match Erlang idiom

--- bowling/erlang/game_happy.erl
+++ bowling/erlang/game_happy.erl
@@ -3,22 +3,22 @@
 -module(game_happy).
 -export([score/1]).
 
-score(Rolls) -> frame(Rolls, 1, 0).
+score(Rolls) -> frame(1, 0, Rolls).
 
 % Game complete.
-frame(_BonusRolls, 11, Score) -> Score;
+frame(11, Score, _BonusRolls) -> Score;
 
 %% Strike.
-frame([10|Rest], Frame, Score) ->
-    frame(Rest, Frame + 1, Score + 10 + strike_bonus(Rest));
+frame(Frame, Score, [10|Rest]) ->
+    frame(Frame + 1, Score + 10 + strike_bonus(Rest), Rest);
 
 %% Spare.
-frame([First,Second|Rest], Frame, Score) when (First + Second == 10) ->
-    frame(Rest, Frame + 1, Score + 10 + spare_bonus(Rest));
+frame(Frame, Score, [First,Second|Rest]) when (First + Second == 10) ->
+    frame(Frame + 1, Score + 10 + spare_bonus(Rest), Rest);
 
 %% Normal.
-frame([First,Second|Rest], Frame, Score) ->
-    frame(Rest, Frame + 1, Score + First + Second).
+frame(Frame, Score, [First,Second|Rest]) ->
+    frame(Frame + 1, Score + First + Second, Rest).
 
 %% spare & strike bonus calculations.
 spare_bonus([First|_Rest]) -> First.
